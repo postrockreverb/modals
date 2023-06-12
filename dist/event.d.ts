2ec1d88162d7089502436256eba22dfc127f9d02
SelenZzz <58228495+SelenZzz@users.noreply.github.com>
2023-06-12 23:29:47 +0000

    Build: (d39d1c3) fix onClose

--- dist/event.d.ts
+++ dist/event.d.ts
@@ -1,2 +1,2 @@
-export declare const activeModalUpdateEventName = "activemodalupdate";
+export declare const ACTIVE_MODAL_UPDATE_EVENT_NAME = "activemodalupdate";
 export declare const collectEvent: () => void;
