ac834e423c0bb2182f50b9a62803a383f1de69c8
SelenZzz <58228495+SelenZzz@users.noreply.github.com>
2023-07-01 22:44:14 +0000

    Build: (e6afc2e) fix historyState on close modal, fix doubling modal param on init, add watcher

--- dist/stack.d.ts
+++ dist/stack.d.ts
@@ -1,5 +1,6 @@
 import { Modal } from './types';
 export declare const getActiveStack: () => Modal[];
+export declare const getActiveModal: () => Modal | null;
 export declare const getPreviousModal: () => Modal | null;
 export declare const isActiveModal: (modalSid: Modal['_sid']) => boolean;
 export declare const isModalOpened: (modalSid: Modal['_sid']) => boolean;
